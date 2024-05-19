f9d2282ae3dc4cf1cb09889d96c0b47174c44289
ZeathDev <ZeathDev@outlook.com>
2024-05-19 21:26:35 +0800

    初始提交

---
+++ killer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE *fp;
+    char buffer[1];
+    int fileSize; // 用户自定义的文件大小（单位：GB）
+
+    printf("File Size(GB): ");
+    scanf("%d", &fileSize);
+    char str[fileSize];
+
+    for (int i = 0; i < fileSize; i++)
+    {
+
+        sprintf(str, "%s%d", "output_file.", i + 1);
+        fp = fopen(str, "w");
+        if (fp == NULL)
+        {
+            printf("Cannot open file\n");
+            return 1;
+        }
+
+        fseek(fp, 1024 * 1024 * 1024 - 1, SEEK_SET); // 指针移动到1GB
+        fwrite(buffer, 1, 1, fp);                    // 写入一个字节
+
+        fclose(fp); // 关闭此次循环生成的文件
+    }
+    return 0;
+}
